ad036303e4c8082c17d9acbd419de0f7a9b139c3
Vadim Kantorov <vadimkantorov@gmail.com>
2025-02-15 19:22:18 +0100

    ...

--- lexical-cli-render-tsx.ts
+++ lexical-cli-render-tsx.ts
@@ -1,26 +1,21 @@
+import { EmojiNode } from "./src/nodes/EmojiNode";
 import { TextNode, $getRoot, $selectAll } from "lexical";
 import { createHeadlessEditor } from "@lexical/headless";
 import { $generateHtmlFromNodes } from "@lexical/html";
 import { JSDOM } from 'jsdom';
-
 import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 import { LinkNode } from "@lexical/link";
 import { HashtagNode } from "@lexical/hashtag";
 import { ListNode, ListItemNode } from "@lexical/list";
-
-import { EmojiNode } from "./src/nodes/EmojiNode";
-
 let input : string = "", html : string = "";
 const editor = createHeadlessEditor({
   editable: false,
   nodes: [TextNode, HeadingNode, QuoteNode, LinkNode, HashtagNode, ListNode, ListItemNode, EmojiNode],
   onError: (error) => { throw error;},
 });
-
 const dom = new JSDOM();
 global.window = dom.window;
 global.document = dom.window.document;
-
 process.stdin.on("data", (chunk) => {  input += chunk; });
 process.stdin.on("end", async () => 
 {
