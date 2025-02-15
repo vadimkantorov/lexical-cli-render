26a4f3b1b8849857d118fce6cbf0bf94abde849d
Vadim Kantorov <vadimkantorov@gmail.com>
2025-02-15 15:29:32 +0100

    ...

---
+++ lexical-cli-render.js
@@ -0,0 +1,37 @@
+import { TextNode, $getRoot, $selectAll } from "lexical";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { LinkNode } from "@lexical/link";
+import { HashtagNode } from "@lexical/hashtag";
+import { ListNode, ListItemNode } from "@lexical/list";
+import { createHeadlessEditor } from "@lexical/headless";
+import { $generateHtmlFromNodes } from "@lexical/html";
+import { JSDOM } from 'jsdom';
+
+let input = "", html = "";
+const editor = createHeadlessEditor({
+  editable: false,
+  nodes: [TextNode, HeadingNode, QuoteNode, LinkNode, HashtagNode, ListNode, ListItemNode],
+  onError: (error) => { throw error;},
+});
+
+const dom = new JSDOM();
+global.window = dom.window;
+global.document = dom.window.document;
+
+process.stdin.on("data", (chunk) => {  input += chunk; });
+process.stdin.on("end", async () => 
+{
+  try
+  {
+    const editorStateJson = JSON.parse(input).editorState;
+    const editorState = editor.parseEditorState(editorStateJson);
+    editor.setEditorState(editorState);    
+    editor.update(() => { html = $generateHtmlFromNodes(editor, $selectAll()); });
+    console.log(html);
+  }
+  catch (error)
+  {
+    console.error("Error processing EditorState JSON:", error);
+    process.exit(1);
+  }
+});
